e6e5eecc8c787518d30151ec9ede586f49e9d288
illlliill0001 <130463128+illlliill0001@users.noreply.github.com>
2023-05-11 21:47:50 +0900

    2023.5.11

---
+++ Cproject/2023.5.10.h
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+#pragma region 자기 참조 구조체
+	//// 자기 자신을 가리키는 포인터가 구조체의 멤버로 들어가 있는 구조
+	//
+	//typedef struct node
+	//{
+	//	int data;
+	//	struct node* next;
+	//}node;
+	//
+
+#pragma endregion
+
+
+	return 0;
+}
+
+#pragma once
