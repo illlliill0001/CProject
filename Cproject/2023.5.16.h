e5949b217fd724ad5aabad303491800aeb38e9ed
illlliill0001 <130463128+illlliill0001@users.noreply.github.com>
2023-05-16 21:45:24 +0900

    2023.5.16 좌표이동함수

---
+++ Cproject/2023.5.16.h
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <conio.h>
+
+#define up 72
+#define left 75
+#define right 77
+#define down 80
+
+int main()
+{
+#pragma region _kb
+
+#pragma endregion
+
+
+
+	return 0;
+
+}
