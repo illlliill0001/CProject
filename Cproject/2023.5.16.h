8cc979769cc6d195233c4d92b3c6bc9854211499
illlliill0001 <130463128+illlliill0001@users.noreply.github.com>
2023-05-17 21:45:34 +0900

    2023.5.17

--- Cproject/2023.5.16.h
+++ Cproject/2023.5.16.h
@@ -1,20 +1,93 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <conio.h>
+#include <Windows.h>
 
 #define up 72
 #define left 75
 #define right 77
 #define down 80
 
+void gotoXY(int x, int y)
+{
+    COORD position = { x,y };
+
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
+
+
+
 int main()
 {
-#pragma region _kb
+#pragma region 좌표이동함수 
+    //
+    //int x = 0; 
+    //int y = 0;
+    //int key = 0;
+    //
+    //while (1) 
+    //{
+    //    gotoxy(x, y);
+    //    printf("★");
+    //    if (_kbhit())
+    //    {
+    //        key = _getch();
+    //    }
+    //
+    //    if (GetAsyncKeyState(VK_LEFT) & 0x8000) { 
+    //        x--;
+    //    }
+    //    if (GetAsyncKeyState(VK_RIGHT) & 0x8000) { //오른쪽
+    //        x++;
+    //    }
+    //    if (GetAsyncKeyState(VK_UP) & 0x8000) { //위
+    //        y--;
+    //    }
+    //    if (GetAsyncKeyState(VK_DOWN) & 0x8000) { //아래
+    //        y++;
+    //    }
+    //    
+    //    
+    //}
+    //
 
 #pragma endregion
 
+#pragma region 문자열 길이 함수(strlen) 
+    // 문자열의 크기를 반환하는 함수입니다.
 
+   ////가장 마지막에 NULL 문자를 포함하지 않고 문자열의 크기를 계산합니다. 
+   //char* string = "visual";
+   //
+   //int size = strlen(string);
+   //
+   //printf("size의 값 : %d", size);
+#pragma endregion
 
-	return 0;
+#pragma region 회문
+   // int flag = 1;
+   //
+   // char * data = "LEVEL";
+   //
+   // for (int i = 0; i < strlen(data) / 2; i++)
+   // {
+   //     if (data[i] != data[strlen(data) - 1 - i])
+   //     {
+   //         flag = 0;
+   //         break;
+   //     }
+   //
+   // }
+   //
+   // if (flag == 1)
+   // {
+   //     printf("회문\n");
+   // }
+   // else
+   // {
+   //     printf("회문이 아닙니다\n");
+   // }
+   //
+#pragma endregion
 
-}
+    return 0;
--- Cproject/소스.c
+++ Cproject/소스.c
@@ -3,93 +3,55 @@
 #include <conio.h>
 #include <Windows.h>
 
-#define up 72
-#define left 75
-#define right 77
-#define down 80
-
-void gotoXY(int x, int y)
-{
-	COORD position = { x,y };
-
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
-}
-
-
-
 int main()
 {
-#pragma region 좌표이동함수 
+#pragma region 파일 입출력 
+
+	// 파일 쓰기
+	// fopen("파일의 이름.확장자",파일모드)
+	//  w : 쓰기
+	//  r : 읽기
+	//  a : append  
+	// FILE* filePtr = fopen("DB.txt", "w");
 	//
-    //int x = 0; 
-    //int y = 0;
-    //int key = 0;
-    //
-    //while (1) 
-    //{
-    //    gotoxy(x, y);
-    //    printf("★");
-    //    if (_kbhit())
-    //    {
-    //        key = _getch();
-    //    }
-    //
-    //    if (GetAsyncKeyState(VK_LEFT) & 0x8000) { 
-    //        x--;
-    //    }
-    //    if (GetAsyncKeyState(VK_RIGHT) & 0x8000) { //오른쪽
-    //        x++;
-    //    }
-    //    if (GetAsyncKeyState(VK_UP) & 0x8000) { //위
-    //        y--;
-    //    }
-    //    if (GetAsyncKeyState(VK_DOWN) & 0x8000) { //아래
-    //        y++;
-    //    }
-    //    
-    //    
-    //}
-    //
+	// fputs("ID\n", filePtr);
+	// fputs("Password\n", filePtr);
+	//
+	// 파일 포인터 닫기
+	// fclose(filePtr);
+
+	int screen = 1;
+
+	// screen = 1 FULL 
+	// screen = 2 window 
+
+	SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
+	while(1)
+	{
+
+
+	FILE* readPtr = fopen("monster.txt", "r");
+
+	char buffer[100000] = { 0, };
+
+	fread(buffer, 1, 100000, readPtr);
+	printf("%s", buffer);
+
+	fclose(readPtr);
+
+	syetem(cls);
+	
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			exit(0);
+		}
+	}
+		
+
 
 #pragma endregion
 
-#pragma region 문자열 길이 함수(strlen) 
-    // 문자열의 크기를 반환하는 함수입니다.
 
-   ////가장 마지막에 NULL 문자를 포함하지 않고 문자열의 크기를 계산합니다. 
-   //char* string = "visual";
-   //
-   //int size = strlen(string);
-   //
-   //printf("size의 값 : %d", size);
-#pragma endregion
 
-#pragma region 회문
-   // int flag = 1;
-   //
-   // char * data = "LEVEL";
-   //
-   // for (int i = 0; i < strlen(data) / 2; i++)
-   // {
-   //     if (data[i] != data[strlen(data) - 1 - i])
-   //     {
-   //         flag = 0;
-   //         break;
-   //     }
-   //
-   // }
-   //
-   // if (flag == 1)
-   // {
-   //     printf("회문\n");
-   // }
-   // else
-   // {
-   //     printf("회문이 아닙니다\n");
-   // }
-   //
-#pragma endregion
-
-	return 0;
 
 }
