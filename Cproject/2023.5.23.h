373acd159cbb058e96cc2bf910856fcf3c1e5997
illlliill0001 <130463128+illlliill0001@users.noreply.github.com>
2023-05-23 21:55:28 +0900

    2023.5.23

---
+++ Cproject/2023.5.23.h
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+
+#define up 72
+#define left 75
+#define right 77
+#define down 80
+
+typedef struct player
+{
+	int x;
+	int y;
+
+};
+// ¹Ì·Î ¸Ê µ¥ÀÌÅÍ
+char maze[11][11];
+// ¹Ì·Î ¸Ê »ý¼º
+void createmaze()
+{
+	//0: ºó°ø°£("  "), 1:º®(¤±), 2:Å»Ãâ±¸(¡Ý) 
+
+	strcpy(maze[0], "1111111111");
+	strcpy(maze[1], "1000011111");
+	strcpy(maze[2], "1011011111");
+	strcpy(maze[3], "1111011111");
+	strcpy(maze[4], "1110000111");
+	strcpy(maze[5], "1110110111");
+	strcpy(maze[6], "1110110111");
+	strcpy(maze[7], "1100110011");
+	strcpy(maze[8], "1110110111");
+	strcpy(maze[9], "1100110002");
+	strcpy(maze[10], "1111111111");
+}
+
+void renderer()
+{
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 11; j++)
+		{
+			printf("%c", maze[i][j]);
+		}
+	}
+
+
+}
+
+int main()
+{
+
+
+	return 0;
+}#pragma once
--- Cproject/소스.c
+++ Cproject/소스.c
@@ -2,54 +2,96 @@
 #include <stdio.h>
 #include <conio.h>
 #include <Windows.h>
+#include <stdlib.h>
 
 #define up 72
 #define left 75
 #define right 77
 #define down 80
 
-typedef struct player
+typedef struct Enemy
 {
 	int x;
 	int y;
-	
-};
-// ¹Ì·Î ¸Ê µ¥ÀÌÅÍ
-char maze[11][11];
-// ¹Ì·Î ¸Ê »ý¼º
-void createmaze()
-{
-	//0: ºó°ø°£("  "), 1:º®(¤±), 2:Å»Ãâ±¸(¡Ý) 
 
-	strcpy(maze[0],  "1111111111");
-	strcpy(maze[1],  "1000011111");
-	strcpy(maze[2],  "1011011111");
-	strcpy(maze[3],  "1111011111");
-	strcpy(maze[4],  "1110000111");
-	strcpy(maze[5],  "1110110111");
-	strcpy(maze[6],  "1110110111");
-	strcpy(maze[7],  "1100110011");
-	strcpy(maze[8],  "1110110111");
-	strcpy(maze[9],  "1100110002");
-	strcpy(maze[10], "1111111111");
+	const char* shape
+}Enemy;
+
+typedef struct Player
+{
+	int x, y;
+
+	const char* shape
+	
+} Player;
+
+
+void gotoxy(int x, int y)
+{
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void renderer()
+void keyboard(Player * ptrplayer)
 {
-	for (int i = 0; i < 11; i++)
+
+	char key = 0;
+	
+	if (_kbhit())
 	{
-		for (int j = 0; j < 11; j++)
+		key = _getch();
+		
+		if (key == -32)
 		{
-			printf("%c", maze[i][j]);
+			key = _getch();
 		}
 	}
+	switch (key)
+	{
+	case left: if (ptrplayer->x < 0) return;
+		ptrplayer->x -= 2;
+		break;
+	case right: if (ptrplayer->x > 30) return;
+		ptrplayer->x += 2;
+		break;
+	}
+}
 
+int randomX()
+{
+	srand(time(NULL));
 
+		int random = rand() % 31;
+
+		if (random % 2 == 1)
+		{
+			random += 1;
+		}
+		return random;
 }
 
 int main()
 {
-	
+	system("mode con cols = 30 line = 25");
+	Player player = { 10, 20 , "¡Ú" };
+	Enemy enemy = { random(), 0 , "¢Â"};
+
+	while (1)
+	{
+		keyboard(&player);
+
+		gotoxy(enemy.x, enemy.y++);
+		printf("%s", enemy.shape);
+
+		gotoxy(player.x, player.y);
+		printf("%s", player.shape);
+
+		sleep(100);
+
+		system("cls");
+
+
+	}
 
 	return 0;
 }
